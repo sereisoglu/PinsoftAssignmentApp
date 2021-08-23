05668bc3952db6fa5a22908e5cc5de89de996d3b
Saffet Emin Reisoğlu <sereisoglu@gmail.com>
2021-08-23 14:51:30 +0300

    :wrench: Set up project

---
+++ PinsoftAssignmentApp/PinsoftAssignmentApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PinsoftAssignmentApp
+//
+//  Created by Saffet Emin Reisoğlu on 23.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
