866e5b9e651761527219ef77309a5a0cb1b9acc7
Saffet Emin Reisoğlu <sereisoglu@gmail.com>
2021-08-24 01:52:28 +0300

    :arrow_up: Updated app delegate and supports files

--- PinsoftAssignmentApp/PinsoftAssignmentApp/sources/AppDelegate.swift
+++ PinsoftAssignmentApp/PinsoftAssignmentApp/sources/AppDelegate.swift
@@ -6,17 +6,19 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
         window = UIWindow()
         window?.makeKeyAndVisible()
-        window?.rootViewController = ViewController()
+        
+        FirebaseApp.configure()
+        
+        window?.rootViewController = RootController()
         
         return true
     }
